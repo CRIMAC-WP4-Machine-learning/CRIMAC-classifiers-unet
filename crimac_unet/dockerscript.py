d9417222ac94c704e486b682400586cab105152b
Ingrid Utseth <iutseth@hotmail.com>
2023-02-22 14:40:54 +0100

    untested attempt at updating dockerscript

--- crimac_unet/dockerscript.py
+++ crimac_unet/dockerscript.py
@@ -1,8 +1,4 @@
 import json
-import setpaths
-import torch
-import numpy as np
-import random
 import time
 import os
 
@@ -13,6 +9,7 @@ PRED_DIR = '/dataout/'
 PRED_FILE = PRED_DIR + os.getenv('ZARRFILE')
 MODELWEIGHTS = '/model/'+os.getenv('MODEL')
 SURVEY = os.getenv('SURVEY')
+CONFIG_FILE = '/configs/config_baseline.yaml'
 
 # The docker mounts the main data folder as /datain/
 print('********************')
@@ -27,50 +24,53 @@ print('Prediction dir:'+PRED_DIR+' exists:'+str(os.path.isdir(PRED_DIR)))
 setpyenv = {
     "syspath": "/crimac_unet/",
     "path_to_echograms":  SV_FILE,
-    "path_to_korona_data": SV_FILE,
-    "path_to_korona_transducer_depths": SV_FILE,
     "path_to_trained_model": MODELWEIGHTS,
-    "path_to_zarr_files": SV_FILE,
-    "path_for_saving_figs": PRED_DIR,
-    "path_for_saving_preds_labels": PRED_FILE}
+    "path_to_zarr_files": SV_FILE}
 
 # Write setpyenv file based on environmental variables
 with open("/crimac_unet/setpyenv.json", "w") as fp:
- json.dump(setpyenv, fp, indent = 4)
+    json.dump(setpyenv, fp, indent=4)
 
 # Set the correct paths to the files
 from paths import *
-from pipeline_train_predict.pipeline import Config_Options, SegPipeUNet, pipeline_config, DataZarr
-from data.data_reader import get_data_readers
+from pipeline_train_predict.pipeline import SegPipeUNet
+from utils.general import (get_argparse_parser, load_yaml_config,
+                           parse_config_options)
+from data.partition import DataZarr
+from pipeline_train_predict.save_predict import save_survey_predictions_zarr
 
 # Check paths
 print(' ')
 print('Check paths:')
-print('path_to_echograms: '+path_to_echograms())
-print('path_to_korona_data: '+path_to_korona_data())
-print('path_to_korona_transducer_depths: '+path_to_korona_transducer_depths())
-print('path_to_trained_model: '+path_to_trained_model())
 print('path_to_zarr_files: '+path_to_zarr_files()+' Content:')
 print(os.listdir(path_to_zarr_files()))
-print('path_for_saving_figs: '+path_for_saving_figs()+' Content:')
-print('path_for_saving_preds_labels: '+path_for_saving_preds_labels())
 
-# Configuration options dictionary
-configuration = pipeline_config()
-configuration['selected_surveys'] = [SURVEY]
+# Configuration options
+argparse_args = get_argparse_parser(mode="save_predict").parse_args()
+configuration = load_yaml_config(argparse_args.yaml_path)
+config_args = parse_config_options(configuration, argparse_args)
+
+config_args['save_prediction_surveys'] = [SURVEY]
 print(' ')
 print('configuration:')
 print(configuration)
 
-# Create options instance
-opt = Config_Options(configuration)
+# Set up data loader and load trained model
+data_partition_object = DataZarr(**config_args)
+experiment_name = config_args["yaml_path"].stem
+segpipe = SegPipeUNet(**config_args, experiment_name=experiment_name)
+segpipe.load_model_params(checkpoint_path=MODELWEIGHTS)
 
-# Set up the code
-data_obj = DataZarr(opt)
-segpipe = SegPipeUNet(opt)
 # Save segmentation predictions
 print("Save predictions")
 start = time.time()
-segpipe.save_segmentation_predictions_zarr(data_obj, resume=opt.resume_writing)
+save_prediction_surveys = config_args["save_prediction_surveys"]
+for survey in save_prediction_surveys:
+    readers = data_partition_object.get_survey_readers(survey)
+    print(f'Saving predictions for survey {survey}, {len(readers)} data reader(s)')
+
+    for reader in readers:
+        target_dname = os.path.join(PRED_DIR, reader.name + '_prediction.zarr')
+        save_survey_predictions_zarr(reader, segpipe, target_dname=target_dname, **config_args)
 
 print(f"Executed time for saving all prediction (h): {np.round((time.time() - start) / 3600, 2)}")
